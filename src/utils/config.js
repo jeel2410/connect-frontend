ed4c904b52cac3cefd1adca183536e03b6a176ac
Nensi Vachhani <nensivachhani04@gmail.com>
2026-02-07 15:02:19 +0530

    chnage config file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 // API Base URL Configuration
-// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://api.conect.in";
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://api.conect.in";
+//const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 
 export default API_BASE_URL;
